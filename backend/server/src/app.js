487a9469d2cc5214e1ac43cddd4d67341a7394cd
Yash Chokhani <yashchokhani95@gmail.com>
2024-07-01 19:15:22 +0530

    feat: middleware and routes added

--- backend/server/src/app.js
+++ backend/server/src/app.js
@@ -2,26 +2,45 @@ require("dotenv").config();
 const app = require("express")();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const { AppError } = require("./middleware/middleware");
+const UserRoutes = require("./routes/userRoutes");
+const ImageRoutes = require("./routes/imageRoutes");
+const DocumentRoutes = require("./routes/documentRoutes");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
-
-mongoose
-  .connect(process.env.MONGODB_DBURL)
-  .then(() => {
-    console.log("connected to mongodb");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
-  res.send("Welcome");
+  res.send("Welcome to the DocHub API ecs version 1.0.0");
 });
 
+app.get("/health", (req, res) => {
+  res.send("ok health");
+});
+console.log("Hello");
+app.use("/user", UserRoutes);
+app.use("/image", ImageRoutes);
+app.use("/docs", DocumentRoutes);
+
 const PORT = process.env.PORT || 5000;
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (err.name === "AppError") {
+    res
+      .status(err.status)
+      .send({ success: false, message: err.message, data: {} });
+    return;
+  }
+  res.status(err.status || 500).send({
+    success: false,
+    message: "Internal Server Error, try after some time",
+    data: {},
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port:${PORT}`);
 });
+
+// module.exports.handler = serverless(app);
