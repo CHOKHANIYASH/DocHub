c1dd2f66d1ff35bde9fa7510dc8d06fefb059315
Yash Chokhani <yashchokhani95@gmail.com>
2024-07-04 02:18:27 +0530

    feat: AllDocuments and updateDocument

--- backend/server/src/controllers/documentControllers.js
+++ backend/server/src/controllers/documentControllers.js
@@ -3,18 +3,44 @@ const { AppError } = require("../middleware/middleware");
 const { response } = require("express");
 const prisma = new PrismaClient();
 const create = async ({ authorId, docId }) => {
-  const newDocument = await prisma.docs.create({
-    data: {
-      authorId,
-      id: docId,
-    },
-  });
+  const newDocument = await prisma.docs
+    .create({
+      data: {
+        authorId,
+        id: docId,
+      },
+    })
+    .catch((err) => {
+      throw new AppError("User not found", 400);
+    });
   return {
     status: 201,
     response: {
       success: true,
       message: "Document created Successfully",
       data: newDocument,
+    },
+  };
+};
+const getAllDocuments = async ({ authorId }) => {
+  const response = await prisma.docs
+    .findMany({
+      where: {
+        authorId,
+      },
+    })
+    .catch((err) => {
+      throw new AppError("User not found", 404);
+    });
+  const documents = response.map((document) => {
+    return { id: document.id, name: document.name };
+  });
+  return {
+    status: 200,
+    response: {
+      success: true,
+      message: "Users documents",
+      data: documents,
     },
   };
 };
@@ -56,4 +82,34 @@ const updateDocumentJson = async ({ docId, json }) => {
     response: { success: true, message: "Json saved successfully", data: {} },
   };
 };
-module.exports = { create, getDoument, updateDocumentJson };
+const updateDocument = async ({ docId, data }) => {
+  const document = await prisma.docs
+    .update({
+      where: {
+        id: docId,
+      },
+      data,
+    })
+    .catch((err) => {
+      console.log(err.code);
+      if (err.code === "P2025")
+        // P2025 Prisma error code
+        throw new AppError("Document not found", 404);
+      else throw err;
+    });
+  return {
+    status: 200,
+    response: {
+      success: true,
+      message: "Document Updated Successfully",
+      data: {},
+    },
+  };
+};
+module.exports = {
+  create,
+  getAllDocuments,
+  getDoument,
+  updateDocument,
+  updateDocumentJson,
+};
--- backend/server/src/routes/documentRoutes.js
+++ backend/server/src/routes/documentRoutes.js
@@ -9,8 +9,18 @@ const { v4: uuid } = require("uuid");
 router.get("/", (req, res) => {
   res.send("Welcome to Documents Routes");
 });
+router.get(
+  "/user/:userId",
+  handleAsyncError(async (req, res) => {
+    const userId = req.params.userId;
+    const { status, response } = await DocumentControllers.getAllDocuments({
+      authorId: userId,
+    });
+    res.status(status).send(response);
+  })
+);
 router.post(
-  "/:userId/create",
+  "/user/:userId/create",
   handleAsyncError(async (req, res) => {
     const userId = req.params.userId;
     const docId = uuid();
@@ -25,6 +35,9 @@ router.get(
   "/:docId",
   handleAsyncError(async (req, res) => {
     const docId = req.params.docId;
+    // const allowedUser = await DocumentControllers.allowedUser({ userId });
+    // if (!allowedUser)
+    //   throw new AppError("You are not allowed to change this document", 403);
     const { status, response } = await DocumentControllers.getDoument({
       docId,
     });
@@ -35,10 +48,30 @@ router.post(
   "/:docId/json",
   handleAsyncError(async (req, res) => {
     const docId = req.params.docId;
-    const json = req.body;
+    const json = req.body.json;
+    const userId = req.body.userId;
+    // const allowedUser = await DocumentControllers.allowedUser({ userId });
+    // if (!allowedUser)
+    //   throw new AppError("You are not allowed to change this document", 403);
     const { status, response } = await DocumentControllers.updateDocumentJson({
       docId,
       json,
+    });
+    res.status(status).send(response);
+  })
+);
+router.post(
+  "/:docId/update",
+  handleAsyncError(async (req, res) => {
+    const docId = req.params.docId;
+    const data = req.body.data;
+    const userId = req.body.userId;
+    // const allowedUser = await DocumentControllers.allowedUser({ userId });
+    // if (!allowedUser)
+    //   throw new AppError("You are not allowed to change this document", 403);
+    const { status, response } = await DocumentControllers.updateDocument({
+      docId,
+      data,
     });
     res.status(status).send(response);
   })
