181d88b1f345699b685d78f53b80a936d929103e
Yash Chokhani <yashchokhani95@gmail.com>
2024-07-04 15:00:26 +0530

    fix: signin/signup signout display

--- frontend/src/app/Navbar.jsx
+++ frontend/src/app/Navbar.jsx
@@ -1,23 +1,34 @@
 "use client";
-
-import React, { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import React, { useState, useEffect } from "react";
 import { HoveredLink, Menu, MenuItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
+import { ToastContainer } from "react-toastify";
+import { useAppSelector } from "@/redux/hooks/index";
 
 function Navbar({ className }) {
   const [active, setActive] = useState(null);
+  const isAuthenticated = useAppSelector((state) => state.isAuthenticated);
   return (
-    <div className={cn("inset-x-0  mx-auto max-md:m-4 ", className)}>
-      <Menu setActive={setActive}>
-        <Link href={"/"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Home"
-          ></MenuItem>
-        </Link>
-        {/* <MenuItem setActive={setActive} active={active} item="Docs">
+    <>
+      <div className={cn("inset-x-0  mx-auto max-md:m-4  ", className)}>
+        <Menu setActive={setActive}>
+          <Link href={"/"}>
+            <MenuItem
+              setActive={setActive}
+              active={active}
+              item="Home"
+            ></MenuItem>
+          </Link>
+          <Link href={"/dashboard/c1431d3a-2051-70f5-ae0d-afb4b87fb800"}>
+            <MenuItem
+              setActive={setActive}
+              active={active}
+              item="Dashboard"
+            ></MenuItem>
+          </Link>
+          {/* <MenuItem setActive={setActive} active={active} item="Docs">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/courses">All Courses</HoveredLink>
             <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
@@ -26,32 +37,45 @@ function Navbar({ className }) {
             <HoveredLink href="/courses">Music Production</HoveredLink>
           </div>
         </MenuItem> */}
-        <Link href={"/login"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Login"
-          ></MenuItem>
-        </Link>
-        <Link href={"/signup"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Signup"
-          ></MenuItem>
-        </Link>
-        <Link href={"/logout"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Logout"
-          ></MenuItem>
-        </Link>
-        <Link href={"/contact"}>
-          <MenuItem setActive={setActive} active={active} item="AI"></MenuItem>
-        </Link>
-      </Menu>
-    </div>
+          {!isAuthenticated ? (
+            <>
+              {" "}
+              <Link href={"/signin"}>
+                <MenuItem
+                  setActive={setActive}
+                  active={active}
+                  item="Signin"
+                ></MenuItem>
+              </Link>
+              <Link href={"/signup"}>
+                <MenuItem
+                  setActive={setActive}
+                  active={active}
+                  item="Signup"
+                ></MenuItem>
+              </Link>
+            </>
+          ) : (
+            <Link href={"/signout"}>
+              <MenuItem
+                setActive={setActive}
+                active={active}
+                item="Signout"
+              ></MenuItem>
+            </Link>
+          )}
+
+          <Link href={"/contact"}>
+            <MenuItem
+              setActive={setActive}
+              active={active}
+              item="AI"
+            ></MenuItem>
+          </Link>
+        </Menu>
+      </div>
+      <ToastContainer autoClose={2000} />
+    </>
   );
 }
 
