4a9145458c83781b93a3787a4399af898ceb1bff
Yash Chokhani <yashchokhani95@gmail.com>
2024-07-16 22:09:21 +0530

    feat: amplify files

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "dochube8394fb6e8394fb6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
