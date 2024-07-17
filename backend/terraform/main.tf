50d67fff93e46bd825eb9c534986f602bd2422b9
Yash Chokhani <yashchokhani95@gmail.com>
2024-07-17 16:34:19 +0530

    feat: lambda service added

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -21,3 +21,48 @@ resource "aws_s3_bucket_policy" "dochub_bucket_policy"{
         ]
     })
 }
+data "aws_iam_policy_document" "lambda_role_policy" {
+    statement {
+      effect = "Allow"
+      principals {
+        type = "Service"
+        identifiers = ["lambda.amazonaws.com"]
+      }
+      actions = ["sts:AssumeRole"]
+    }
+}
+resource "aws_iam_role" "dochub_lambda_role" {
+  name = "dochub_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_role_policy.json
+}
+resource "aws_lambda_function" "dochub_server" {
+  function_name = "dochub_server"
+  role = aws_iam_role.dochub_lambda_role.arn
+  package_type = "Image"
+  image_uri = "225542105072.dkr.ecr.ap-south-1.amazonaws.com/dochub:dochub-server"
+}
+resource "aws_cloudwatch_log_group" "lambda_log_group_dochub" { // CloudWatch log group creation for image resizer lambda
+  name              = "/aws/lambda/${aws_lambda_function.dochub_server.function_name}"
+  retention_in_days = 14
+}
+data "aws_iam_policy_document" "lambda_logs_policy" { // IAM policy for lambda to write logs to CloudWatch data
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+resource "aws_iam_policy" "dochub_lambda_logs_policy" { // IAM policy for lambda to write logs to CloudWatch
+  name        = "dochub_lambda_logs_policy"
+  description = "IAM policy for lambda to write logs to CloudWatch"
+  policy      = data.aws_iam_policy_document.lambda_logs_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" { // IAM role policy attachment for lambda to write logs to CloudWatch
+  role       = aws_iam_role.dochub_lambda_role.name
+  policy_arn = aws_iam_policy.dochub_lambda_logs_policy.arn
+}
