0767b0d639a295fd8d641aa0b13583f2a030a7e1
Yash Chokhani <yashchokhani95@gmail.com>
2024-09-07 02:44:42 +0530

    feat: Logo added to home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,15 +1,19 @@
 "use client";
 import "aws-amplify/auth/enable-oauth-listener";
 import GridBackground from "./GridBackground";
+import Image from "next/image";
 export default function Home() {
   return (
     <>
       <GridBackground>
+        <Image
+          className="mb-10 ml-auto mr-auto text-center"
+          src="/icon1.png"
+          width={200}
+          height={200}
+        />
         Welcome to DocHub <br /> A perfect place to create and share documents{" "}
         <br />
-        <h2 className="text-xl">
-          Test User(username = testUser, password = Test#123)
-        </h2>
       </GridBackground>
     </>
   );
